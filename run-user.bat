28ac8da6cba45c952078439f177452e5f7da72c9
Murlors <1595258509@qq.com>
2023-12-31 18:52:18 +0800

    🦄 refactor: Refactor project name and update README files.
    - Updated project name from 'WZU-SPOC-tools' to 'CNMOOC-Tools'.
    - Improved project description in both English and Chinese READMEs.

--- run-user.bat
+++ run-user.bat
@@ -12,6 +12,11 @@ set GIT=
 :: Virtual environment path
 set VENV_DIR=
 :: Command line arguments
-set COMMANDLINE_ARGS=--webdriver_manager --headless
+:: options:
+::   -wm, --webdriver_manager   是否使用webdriver_manager
+::   --headless                 是否使用无头模式
+::   --update_anyway            是否强制更新题库
+::   --cnmooc                   是否使用cnmooc登录
+set COMMANDLINE_ARGS=--webdriver_manager --headless --cnmooc
 
 call run.bat
